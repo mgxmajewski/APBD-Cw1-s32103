fe1c0779191884402a272e656873a016f9dc6d5c
mgx <60788689+mgxmajewski@users.noreply.github.com>
2026-03-30 20:12:53 +0200

    Fix - powtórzone klasy

--- RentalRules.cs
+++
@@ -1,8 +0,0 @@
-namespace APBD_Cw1_s32103;
-
-public static class RentalRules
-{
-    public const int StudentMaxRentals = 2;
-    public const int EmployeeMaxRentals = 5;
-    public const decimal PenaltyPerDay = 15.00m;
-}
--- Student.cs
+++
@@ -1,8 +0,0 @@
-namespace APBD_Cw1_s32103;
-
-public class Student : User
-{
-    public override int MaxActiveRentals => RentalRules.StudentMaxRentals;
-
-    public Student(string firstName, string lastName) : base(firstName, lastName) { }
-}
